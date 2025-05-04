2eceb159e8dbbb23f97571a63222043455646dd9
DxMaurine <103417641+DxMaurine@users.noreply.github.com>
2025-05-05 00:03:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function validateAccess() {
     const accessKey = document.getElementById('accessKey').value;
-    const correctKey = "12345"; // Ganti dengan akses key rahasia kamu
+    const correctKey = "20250505"; // Ganti dengan akses key rahasia kamu
 
     if (accessKey === correctKey) {
         alert("Akses diterima!");
-        localStorage.setItem("accessKey", accessKey); // Simpan akses key
+        sessionStorage.setItem("accessKey", accessKey); // Simpan akses key di sessionStorage
         window.location.href = "index.html"; // Redirect ke halaman utama
     } else {
         alert("Akses key salah!");
     }
 }
+
 
